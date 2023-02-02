762a0bf54e743ba4b394195346d1ed7094ce41dd
Shakil Khan <67122643+Shakil10945@users.noreply.github.com>
2023-02-02 21:35:41 +0600

    Update 04-insertionAtTail.cpp

--- 04-insertionAtTail.cpp
+++ 04-insertionAtTail.cpp
@@ -14,6 +14,8 @@ public:
 };
 void insertAtHead(Node* &head, int value);
 void insertAtTail(Node* &head, int value);
+void display(Node* head);
+int countSize(Node* head);
 
 void insertAtHead(Node* &head, int value)
 {
@@ -43,12 +45,46 @@ void insertAtTail(Node* &head, int value)
     }
 }
 
+void display(Node* head)
+{
+    if(head==NULL)
+    {
+        cout<< "Sorry the Linked list is empty right now";
+    }
+    Node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->value;
+        if(temp->Next!=NULL)
+        {
+            cout<< "->";
+        }
+        temp=temp->Next;
+    }
+    cout<<endl;
+}
+
+int countSize(Node* head)
+{
+    Node *temp=head;
+    int size=0;
+    while(temp!=NULL)
+    {
+        temp=temp->Next;
+        size++;
+    }
+    return size;
+}
+
+
 int main()
 {
     Node *head=NULL;
     int value;
     int choice;
     cout<< "Choice 1: for entering the value into head"<<endl
+        << "Choice 2: for entering the value into tail"<<endl
+        << "Choice 3: for watching the size of the linked list"<<endl
 
         << "Choice 0: for exit"<<endl
         <<endl;
@@ -69,6 +105,13 @@ int main()
             cin>>value;
             insertAtTail(head,value);
             break;
+        case 3:
+            cout<< "Size of this Linked list is ";
+            int size=countSize(head);
+            cout<< size<<endl;
+            break;
+
+
         }
 
 
@@ -77,6 +120,7 @@ int main()
         cout<< "Enter your Next Choice : ";
         cin>> choice;
     }
+    display(head);
 
     return 0;
 }
